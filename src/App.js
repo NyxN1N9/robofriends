d5f8a60b9bf71e76933080bec64c8bfdd6db1334
Chaos.Nyx <Chaos@chaos648@gmail.com>
2023-09-02 16:59:25 -0500

    class comp

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  render(){
+    return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+    );
+  }
+}
+
+
+/* function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -21,5 +46,5 @@ function App() {
     </div>
   );
 }
-
+ */
 export default App;
