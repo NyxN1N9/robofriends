70e4c5e30675d898107914009c24ffbeff673b83
Chaos.Nyx <Chaos@chaos648@gmail.com>
2023-09-03 09:32:20 -0500

    -unnecessary files

--- src/index.js
+++ src/index.js
@@ -1,26 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Card from './Card';
 import reportWebVitals from './reportWebVitals';
-import Hello from './Hello';
 import 'tachyons';
 
-/* 
+/* (alternate code)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1> Hello World </h1>
+    <h1>Hello World</h1>
     <App />
   </React.StrictMode>
 ); 
 */
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Hello greeting={'Hello ' + 'React Ninja'}/> 
+  <Card greeting={'Hello React Ninja'}/>
 );
 
 reportWebVitals(); 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
