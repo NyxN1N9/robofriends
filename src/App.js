dd65a6caaf63d19eaad750cddc22a0f466733dcd
Chaos.Nyx <Chaos@chaos648@gmail.com>
2023-09-03 20:11:58 -0500

    chg h1 + dl font

--- src/App.js
+++ src/App.js
@@ -1,35 +1,38 @@
 import React, { Component } from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
-import {robots} from './robots';
+import CardList from "./CardList";
+import SearchBox from "./SearchBox";
+import { robots } from "./robots";
+import './App.css';
 
-class App extends Component{
-  constructor(){
-    super()
+class App extends Component {
+  constructor() {
+    super();
     this.state = {
       robots: robots,
-      searchfield: ''
+      searchfield: '',
     }
   }
 
   onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value })
-  }
+    this.setState({ searchfield: event.target.value });
+  };
 
-  render(){
-    const filteredRobots = this.state.robots.filter(robot=>{
-      return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-    })
-      return (
+  render() {
+    const filteredRobots = this.state.robots.filter((robot) => {
+      return robot.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase());
+    });
+    return (
       <React.Fragment>
-        <div className='tc'>
-          <h1>RoboFriends</h1>
-          <SearchBox searchChange={this.onSearchChange}/>
-          <CardList robots={filteredRobots}/>
+        <div className="tc">
+          <h1 className='f1'>RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList robots={filteredRobots} />
         </div>
-      </React.Fragment>    
+      </React.Fragment>
     );
-  } 
+  }
 }
 
 export default App;
