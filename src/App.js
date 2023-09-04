8ae2fb74adcf8ea281a0af5b2db3d2f23754b777
Chaos.Nyx <Chaos@chaos648@gmail.com>
2023-09-03 22:08:46 -0500

    +scroll

--- src/App.js
+++ src/App.js
@@ -1,37 +1,51 @@
 import React, { Component } from 'react';
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
-import { robots } from "./robots";
+import Scroll from './Scroll';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [], 
       searchfield: '',
     }
   }
 
+componentDidMount(){
+  fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ robots: users}));
+    //.then(users => {});
+}
+
   onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value });
-  };
+    this.setState({ searchfield: event.target.value })
+  }
 
   render() {
     const filteredRobots = this.state.robots.filter((robot) => {
       return robot.name
         .toLowerCase()
         .includes(this.state.searchfield.toLowerCase());
-    });
-    return (
-      <React.Fragment>
-        <div className="tc">
-          <h1 className='f1'>RoboFriends</h1>
-          <SearchBox searchChange={this.onSearchChange} />
-          <CardList robots={filteredRobots} />
-        </div>
-      </React.Fragment>
-    );
+    })
+    if (this.state.robots.length === 0) {
+      return <h1 className='tc'>Loading...</h1>
+    } else {
+      return (
+        <React.Fragment>
+          <div className="tc">
+            <h1 className='f1'>RoboFriends</h1>
+            <SearchBox searchChange={this.onSearchChange} />
+            <Scroll>
+              <CardList robots={filteredRobots} />
+            </Scroll>
+            
+          </div>
+        </React.Fragment>
+      );
+    }
   }
 }
 
