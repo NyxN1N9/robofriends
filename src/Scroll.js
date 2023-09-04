68334a0d496d78a86327f9e2c307d81a4670dad7
Chaos.Nyx <Chaos@chaos648@gmail.com>
2023-09-03 22:12:21 -0500

    +props scroll

--- src/Scroll.js
+++ src/Scroll.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
 const Scroll = (props) => {
-    console.log(props);
-    return props.children
+    return (
+        <div style={{ overflowY: 'scroll', border: 'ipx solid black', height: '500px' }}>
+        {props.children}
+        </div>
+    );
 };
 
 export default Scroll;
